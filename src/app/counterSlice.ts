9c9761116b0ea99074c888213419b36f34b4c5e1
Busra Mestan <bmestan50@gmail.com>
2025-08-12 03:13:08 +0300

    Initial commit: React Redux counter functionality

---
+++ src/app/counterSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+const initialState = {
+    value: 0,
+}
+
+const counterSlice = createSlice({
+    name: "counter",
+    initialState: initialState,
+    reducers : { //yapılacak olan işlemler (actions)
+        increment(state) {
+            state.value += 1;
+        },
+        decrement(state) {
+            if(state.value == 0) {
+                return;
+            }
+            state.value -= 1;
+        },
+        reset: () => initialState,
+        incrementByAmount(state, action: PayloadAction<number>) {  //number titpnde bi şey gönderyoruz. Payload REduxtan geliyor.
+            state.value += action.payload
+        }
+    }
+})
+
+//export const increment = counterSlice.actions.increment;
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
+export default counterSlice.reducer;  //store da kullanncakç 
