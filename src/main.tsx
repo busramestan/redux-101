62b87465196799cdf770c52b86f2d4d98d5f64f8
Busra Mestan <bmestan50@gmail.com>
2025-08-13 05:54:09 +0300

    fix: unnecesseray import removed

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "app/app";
 import { store } from "app/store";
 import { Provider } from "react-redux";
-import Counter from "app/counter";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
